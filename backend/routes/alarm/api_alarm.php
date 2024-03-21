2162db16c9aecb667bf357e33a31c6b06557efc2
Venu J <149071174+akildevs1004@users.noreply.github.com>
2024-03-21 16:04:48 +0400

    Live status

--- backend/routes/alarm/api_alarm.php
+++ backend/routes/alarm/api_alarm.php
@@ -7,15 +7,11 @@ use App\Http\Controllers\AnnouncementsCategoriesController;
 use App\Models\Alarm\DeviceSensorLogs;
 use Illuminate\Support\Facades\Route;
 
-// announcement
-//Route::apiResource('announcement', AnnouncementController::class);
-// Route::get('announcement_list', [AnnouncementController::class, 'annoucement_list']);
-// Route::get('announcement/search/{key}', [AnnouncementController::class, 'search']);
-//Route::get('alarm_device_status', [ApiAlarmControlController::class, 'LogDeviceStatus']);
-Route::post('alarm_device_status', [ApiAlarmControlController::class, 'LogDeviceStatus']);
-//Route::get('announcement/employee/{id}', [AnnouncementController::class, 'getAnnouncement']);
-Route::get('alarm_dashboard_update_responsetime', [ApiAlarmControlController::class, 'updateAlarmResponseTime']);
 
+Route::post('alarm_device_status', [ApiAlarmControlController::class, 'LogDeviceStatus']);
+
+Route::get('alarm_dashboard_update_responsetime', [ApiAlarmControlController::class, 'updateAlarmResponseTime']);
+Route::post('alarm_device_health', [ApiAlarmControlController::class, 'updateDeviceHealth']);
 
 
 Route::get('alarm_dashboard_get_temparature_latest', [DeviceSensorLogsController::class, 'getDeviceLatestTemperature']);
@@ -23,22 +19,7 @@ Route::get('alarm_dashboard_get_hourly_data', [DeviceSensorLogsController::class
 Route::get('alarm_dashboard_get_monthly_data', [DeviceSensorLogsController::class, 'getDeviceTodayMonthlyAlarms']);
 Route::get('alarm_dashboard_get_categories_data', [DeviceSensorLogsController::class, 'getDeviceCategoryAlarms']);
 Route::get('alarm_dashboard_get_devices_data', [DeviceSensorLogsController::class, 'getDeviceDeviceAlarms']);
-
-
-
-
-
-
-
-
 Route::get('alarm_dashboard_get_statistics', [DeviceSensorLogsController::class, 'getDeviceAlarmsStatics']);
-
-
-
-
-
-
-
 Route::get('alarm_dashboard_get_humidity_hourly_data', [DeviceSensorLogsController::class, 'getDeviceTodayHourlyHumidity']);
 Route::get('alarm_device_logs', [DeviceSensorLogsController::class, 'getDeliveLogs']);
 Route::get('alarm_reports', [DeviceSensorLogsController::class, 'getAlarmReports']);
